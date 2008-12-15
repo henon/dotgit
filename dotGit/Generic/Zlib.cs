352af19443c7a89b0f0cb4a4848d60cf707a5e22
Pheew <nbruins@gmail.com>
2008-12-15 23:50:10 +0100

    Fixed Tag parsing regression bug

--- dotGit/Generic/Zlib.cs
+++ dotGit/Generic/Zlib.cs
@@ -5,39 +5,41 @@ using System.Text;
 using System.IO;
 using zlib;
 
+
+
 namespace dotGit.Objects
 {
-	public class Zlib
-	{
-		public static MemoryStream Decompress(string path)
-		{
-			return Decompress(File.OpenRead(path));
-		}
+  public class Zlib
+  {
+    public static MemoryStream Decompress(string path)
+    {
+      return Decompress(File.OpenRead(path));
+    }
 
-		public static MemoryStream Decompress(Stream input)
-		{
-			var output = new MemoryStream();
-			var zipStream = new zlib.ZOutputStream(output);
+    public static MemoryStream Decompress(Stream input)
+    {
+      var output = new MemoryStream();
+      var zipStream = new ZOutputStream(output);
 
-			using (input)
-			{
-				var buffer = new byte[2000];
-				int len;
+      using (input)
+      {
+        var buffer = new byte[2000];
+        int len;
 
-				while ((len = input.Read(buffer, 0, 2000)) > 0)
-				{
-					zipStream.Write(buffer, 0, len);
-				}
-			}
+        while ((len = input.Read(buffer, 0, 2000)) > 0)
+        {
+          zipStream.Write(buffer, 0, len);
+        }
+      }
 
-			output.Position = 0;
+      output.Position = 0;
 
-			byte[] content = new byte[output.Length];
+      byte[] content = new byte[output.Length];
 
-			output.Read(content, 0, (int)output.Length);
+      output.Read(content, 0, (int)output.Length);
 
-			return output;
-		}
+      return output;
+    }
 
     public static byte[] Decompress(byte[] input)
     {
@@ -60,24 +62,25 @@ namespace dotGit.Objects
       }
     }
 
-		public static MemoryStream Decompress(BinaryReader input, long destLength)
-		{
-			int bufferLength = 1;
-
-			MemoryStream output = new MemoryStream();
-			ZOutputStream zipStream = new ZOutputStream(output);
-			
-			byte[] buffer = new byte[bufferLength];
-
-			while (output.Length < destLength)
-			{
-				input.Read(buffer, 0, bufferLength);
-				zipStream.Write(buffer, 0, bufferLength);
-			}
+    public static MemoryStream Decompress(BinaryReader input, long destLength)
+    {
+      int bufferLength = 4;
 
 
-			return output;
-		}
+      MemoryStream output = new MemoryStream();
+      ZOutputStream zipStream = new ZOutputStream(output);
 
-	}
+      byte[] buffer = new byte[bufferLength];
+
+      while (output.Length < destLength)
+      {
+        input.Read(buffer, 0, bufferLength);
+        zipStream.Write(buffer, 0, bufferLength);
+      }
+
+
+      return output;
+    }
+
+  }
 }
--- dotGit/Refs/Tag.cs
+++ dotGit/Refs/Tag.cs
@@ -11,142 +11,139 @@ using dotGit.Generic;
 
 namespace dotGit.Refs
 {
-	/// <summary>
-	/// Represents a tag in the repository. Tags can reference any IStorableObject in the repository
-	/// </summary>
-	public class Tag : Ref, IStorableObject
-	{
-		internal Tag(Repository repo, string sha)
-			:base(repo, sha)
-		{	}
+  /// <summary>
+  /// Represents a tag in the repository. Tags can reference any IStorableObject in the repository
+  /// </summary>
+  public class Tag : Ref, IStorableObject
+  {
+    internal Tag(Repository repo, string sha)
+      : base(repo, sha)
+    { }
 
-		internal static Tag Load(Repository repo, string sha, string path)
-		{
-			if (!Utility.IsValidSHA(sha))
-				throw new ParseException("Tag does not contain valid sha");
+    internal static Tag Load(Repository repo, string sha, string path)
+    {
+      if (!Utility.IsValidSHA(sha))
+        throw new ParseException("Tag does not contain valid sha");
 
-			IStorableObject obj = repo.Storage.GetObject(sha);
+      IStorableObject obj = repo.Storage.GetObject(sha);
 
-			// This is kind of flaky but for now the only way we can distinguish a regular and a lightweight tag
-			if (obj is Tag)
-			{
-				((Tag)obj).Path = path;
-				return (Tag)obj;
-			}
-			else
-			{
-				Tag t = new Tag(repo, sha);
-				t.Object = obj;
-				t.Path = path;
-				return t;
-			}
-		}
+      // This is kind of flaky but for now the only way we can distinguish a regular and a lightweight tag
+      if (obj is Tag)
+      {
+        ((Tag)obj).Path = path;
+        return (Tag)obj;
+      }
+      else
+      {
+        Tag t = new Tag(repo, sha);
+        t.Object = obj;
+        t.Path = path;
+        return t;
+      }
+    }
 
-		/// <summary>
-		/// The IStorableObject this tag references
-		/// </summary>
-		public IStorableObject Object
-		{
-			get;
-			private set;
-		}
+    /// <summary>
+    /// The IStorableObject this tag references
+    /// </summary>
+    public IStorableObject Object
+    {
+      get;
+      private set;
+    }
 
-		/// <summary>
-		/// The contributer that created this tag
-		/// </summary>
-		public Contributer Tagger
-		{
-			get;
-			private set;
-		}
-
-
-		/// <summary>
-		/// The date this tag was made
-		/// </summary>
-		public DateTime TagDate
-		{
-			get;
-			private set;
-		}
-
-		public string TaggedObjectSHA
-		{
-			get;
-			private set;
-		}
-
-		/// <summary>
-		/// The message for this tag
-		/// </summary>
-		public string Message
-		{
-			get;
-			private set;
-		}
-
-		/// <summary>
-		/// Returns true if this tag has a message. Returns false if this is a 'lightweight' tag
-		/// </summary>
-		public bool IsAnnotated
-		{
-			get { return !String.IsNullOrEmpty(Message) && Tagger != null && TagDate != null; }
-		}
+    /// <summary>
+    /// The contributer that created this tag
+    /// </summary>
+    public Contributer Tagger
+    {
+      get;
+      private set;
+    }
 
 
-		/// <summary>
-		/// Loads the tag from the GitObjectReader
-		/// </summary>
-		/// <param name="input">A reader with inflated tag contents</param>
-		public void Deserialize(GitObjectReader input)
-		{
-			string sha;
-			if (Utility.IsValidSHA(input.GetString(20), out sha))
-			{ // Tag contains a regular SHA so we can assume it's an IStorableObject
-				Object = Repo.Storage.GetObject(sha);
-				return;
-			}
-			else
-			{
-				input.Rewind();
+    /// <summary>
+    /// The date this tag was made
+    /// </summary>
+    public DateTime TagDate
+    {
+      get;
+      private set;
+    }
 
-				// Skip header
-				input.ReadToNull();
+    public string TaggedObjectSHA
+    {
+      get;
+      private set;
+    }
 
-				// Skip object keyword
-				input.ReadWord();
+    /// <summary>
+    /// The message for this tag
+    /// </summary>
+    public string Message
+    {
+      get;
+      private set;
+    }
 
-				TaggedObjectSHA = input.ReadLine().GetString().Trim();
-				if (!Utility.IsValidSHA(TaggedObjectSHA))
-					throw new ParseException("Invalid sha from tag content");
+    /// <summary>
+    /// Returns true if this tag has a message. Returns false if this is a 'lightweight' tag
+    /// </summary>
+    public bool IsAnnotated
+    {
+      get { return !String.IsNullOrEmpty(Message) && Tagger != null && TagDate != null; }
+    }
 
-				// Load object; a ParseException will be thrown for unknown types
-				Object = Repo.Storage.GetObject(TaggedObjectSHA);
 
-				// Skip type and tag
-				input.ReadLine(); input.ReadLine();
+    /// <summary>
+    /// Loads the tag from the GitObjectReader
+    /// </summary>
+    /// <param name="input">A reader with inflated tag contents</param>
+    public void Deserialize(GitObjectReader input)
+    {
+      string sha;
+      if (Utility.IsValidSHA(input.GetString(20), out sha))
+      { // Tag contains a regular SHA so we can assume it's an IStorableObject
+        Object = Repo.Storage.GetObject(sha);
+        return;
+      }
+      else
+      {
+        input.Rewind();
 
-				// Tagger
-				input.ReadWord();
-				string taggerLine = input.ReadLine().GetString();
-				TagDate = Utility.StripDate(taggerLine, out taggerLine);
-				Tagger = Contributer.Parse(taggerLine);
+        // Skip object keyword
+        input.ReadWord();
 
-				//Skip extra '\n' and read message
-				input.ReadBytes(1);
-				Message = input.ReadToEnd().GetString().TrimEnd();
+        TaggedObjectSHA = input.ReadLine().GetString().Trim();
+        if (!Utility.IsValidSHA(TaggedObjectSHA))
+          throw new ParseException("Invalid sha from tag content");
 
-			}
-		}
+        // Load object; a ParseException will be thrown for unknown types
+        Object = Repo.Storage.GetObject(TaggedObjectSHA);
 
-		internal override void Deserialize()
-		{
-			throw new NotImplementedException();
-		}
+        // Skip type and tag
+        input.ReadLine(); input.ReadLine();
 
-		public byte[] Serialize()
-		{
-			throw new NotImplementedException();
-		}
-	}
+        // Tagger
+        input.ReadWord();
+        string taggerLine = input.ReadLine().GetString();
+        TagDate = Utility.StripDate(taggerLine, out taggerLine);
+        Tagger = Contributer.Parse(taggerLine);
+
+        //Skip extra '\n' and read message
+        input.ReadBytes(1);
+        Message = input.ReadToEnd().GetString().TrimEnd();
+
+      }
+    }
+
+    internal override void Deserialize()
+    {
+      throw new NotImplementedException();
+    }
+
+    public byte[] Serialize()
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
