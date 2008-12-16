81838031f1cf4bc429fc3ac2e0fd77c5a647a1dd
Mark <sjonge@hotmail.com>
2008-12-16 23:32:06 +0100

    - Added first Configuration tests which dictates the initial api structure - Updated the test repository to reflect some of the properties needed for testing

---
+++ Test/ConfigTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using dotGit;
+using System.IO;
+using dotGit.Objects;
+using dotGit.Refs;
+
+
+namespace Test
+{
+	[TestFixture]	
+	public class ConfigTests
+  {
+    [Test]
+    public void ConfigUserNameShouldBeTest()
+    {
+      Repository repo = Repository.Open(Global.TestRepositoryPath);
+      Assert.IsTrue(String.Equals(repo.Config.User.Name, "Test"), "Username configuration in repository should be 'Test'");
+    }
+
+    [Test]
+    public void ConfigUserEmailShouldBeTestEmail()
+    {
+      Repository repo = Repository.Open(Global.TestRepositoryPath);
+      Assert.IsTrue(String.Equals(repo.Config.User.Email, "test@test.com"), "User email should be test@test.com");
+    }
+
+    [Test]
+    public void OriginShouldBeTestRepositoryAtGitHub()
+    {
+      Repository repo = Repository.Open(Global.TestRepositoryPath);
+      Assert.IsTrue(String.Equals(repo.Remotes.Origin.Url, "git@github.com:pheew/dotgittestrepo.git"), "Repository is not 'origin'ated at github test repository");
+    }
+
+
+	}
+}
--- Test/RepositoryWriteTests.cs
+++ Test/RepositoryWriteTests.cs
@@ -10,27 +10,7 @@ namespace Test
   [TestFixture]	
   class RepositoryWriteTests
   {
-    private string _fullTempTestRepoName = null;
-    private string _tempTestRepoName = @"Resources\UtilityTestTempRepoPath";
-
-    [SetUp]
-    public void Setup()
-    {
-      _fullTempTestRepoName = Path.Combine(Global.AssemblyDir, _tempTestRepoName);
-
-      if (Directory.Exists(_fullTempTestRepoName))
-        Directory.Delete(_fullTempTestRepoName, true);
-
-      Directory.CreateDirectory(_fullTempTestRepoName);
-    }
-
-    [TearDown]
-    public void TearDown()
-    {
-      if (Directory.Exists(_fullTempTestRepoName))
-        Directory.Delete(_fullTempTestRepoName, true);
-    }
-
+  
     [Test]
     public void ObjectWriteShouldReturnTrue()
     {
