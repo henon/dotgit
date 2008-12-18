9f2f22e76d2de8fa54d75f7eceab390c14f56c71
Mark <sjonge@hotmail.com>
2008-12-18 23:14:25 +0100

    Added a first version of the DiffEngine tests

---
+++ dotGit/DiffEngine/Diff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dotGit.DiffEngine
+{
+  public class Diff
+  {
+    public static Diff Compare(string path1, string path2)
+    {
+      throw new NotImplementedException("Method not implemented");
+    }
+  }
+}
