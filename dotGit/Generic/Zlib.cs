a86bcb8446cf98af64af44783f2fed48169ee0c4
Markie2 <m.gerrits@diract-it.nl>
2008-12-31 17:23:27 +0100

    Replaced the zlib.net library with the ICSharpCode library. This seems to perform a lot better.

--- dotGit/Generic/Zlib.cs
+++ dotGit/Generic/Zlib.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using zlib;
 using dotGit.Objects.Storage;
-
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 
 
 namespace dotGit.Objects
@@ -14,22 +14,26 @@ namespace dotGit.Objects
   {
     public static MemoryStream Decompress(string path)
     {
-      return Decompress(File.OpenRead(path));
+      return Decompress(new GitObjectReader(File.OpenRead(path)));
     }
 
-    public static MemoryStream Decompress(Stream input)
+    public static MemoryStream Decompress(BinaryReader input)
     {
       var output = new MemoryStream();
-      var zipStream = new ZOutputStream(output);
+      var zipStream = new Inflater();
 
       using (input)
       {
         var buffer = new byte[2000];
         int len;
 
-        while ((len = input.Read(buffer, 0, 2000)) > 0)
+        while (input.BaseStream.Position < input.BaseStream.Length)
         {
-          zipStream.Write(buffer, 0, len);
+          if (zipStream.IsNeedingInput)
+            zipStream.SetInput(input.ReadBytes(buffer.Length));
+
+          zipStream.Inflate(buffer);
+          output.Write(buffer, 0, buffer.Length);
         }
       }
 
@@ -46,7 +50,7 @@ namespace dotGit.Objects
     {
       using (MemoryStream output = new MemoryStream())
       {
-        using (ZOutputStream zipStream = new ZOutputStream(output))
+        using (InflaterInputStream zipStream = new InflaterInputStream(output))
         {
           using (MemoryStream inputStream = new MemoryStream(input))
           {
@@ -65,19 +69,31 @@ namespace dotGit.Objects
 
     public static MemoryStream Decompress(GitObjectReader input, long destLength)
     {
-      int bufferLength = 4;
+      int bufferLength = 1024;
 
 
       MemoryStream output = new MemoryStream();
-      ZOutputStream zipStream = new ZOutputStream(output);
+      Inflater inflater = new Inflater();
 
-      byte[] buffer = new byte[bufferLength];
+      byte[] buffer = new byte[destLength];
 
       while (output.Length < destLength)
       {
-        input.Read(buffer, 0, bufferLength);
-        zipStream.Write(buffer, 0, bufferLength);
+        if (inflater.IsNeedingInput)
+          inflater.SetInput(input.ReadBytes(bufferLength));
+
+        int outLength = inflater.Inflate(buffer);
+
+        if (outLength > 0)
+          output.Write(buffer, 0, outLength);
+        else
+        {
+          if (inflater.IsFinished)
+            break;
+        }
       }
+
+      input.Position -= inflater.RemainingInput;
 
 
       return output;
