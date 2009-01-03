7c1206e1a0f1fa6e2f9c76dce351811f7ec2cafd
Pheew <nbruins@gmail.com>
2009-01-03 14:52:21 +0100

    Re-align and add some methods docs

--- dotGit/Objects/Storage/Pack.cs
+++ dotGit/Objects/Storage/Pack.cs
@@ -10,48 +10,61 @@ using System.Runtime.InteropServices;
 
 namespace dotGit.Objects.Storage
 {
-	public abstract class Pack
-	{
-		private Pack() { }
+  public abstract class Pack
+  {
+    private Pack() { }
 
-		internal Pack(Repository repo, string path)
-		{
-			Repo = repo;
-			Path = path.TrimEnd(IO.Path.DirectorySeparatorChar);			
-		}
+    internal Pack(Repository repo, string path)
+    {
+      Repo = repo;
+      Path = path.TrimEnd(IO.Path.DirectorySeparatorChar);
+    }
 
-		
-		public static Pack LoadPack(Repository repo, string path)
-		{
-			// TODO: Add version parsing to instantiate the right Pack version. Much like GetObject in ObjectStorage
-			return new PackV2(repo, path);
-		}
-		
 
-		public int NumberOfObjects
-		{
-			get;
-			protected set;
-		}
+    public static Pack LoadPack(Repository repo, string path)
+    {
+      // TODO: Add version parsing to instantiate the right Pack version. Much like GetObject in ObjectStorage
+      return new PackV2(repo, path);
+    }
 
-		public abstract int Version
-		{
-			get;
-		}
+    /// <summary>
+    /// Number of objects in this pack
+    /// </summary>
+    public int NumberOfObjects
+    {
+      get;
+      protected set;
+    }
 
-		public string Path
-		{
-			get;
-			private set;
-		}
+    /// <summary>
+    /// The version of this pack
+    /// </summary>
+    public abstract int Version
+    {
+      get;
+    }
 
-		protected Repository Repo
-		{
-			get;
-			private set;
-		}
+    /// <summary>
+    /// The path to this pack
+    /// </summary>
+    public string Path
+    {
+      get;
+      private set;
+    }
 
-		public abstract IStorableObject GetObject(string sha);
-		
-	}
+    protected Repository Repo
+    {
+      get;
+      private set;
+    }
+
+    /// <summary>
+    /// Searches for an object by it's sha in this pack and returns it as an IStorableObject
+    /// </summary>
+    /// <param name="sha">The sha of the object to find</param>
+    /// <returns>An IStorableObject</returns>
+    public abstract IStorableObject GetObject(string sha);
+
+  }
 }
