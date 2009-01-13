5b37d4daffa3b427cadfcfccaa3ee6a52c48d6ee
Pheew <nbruins@gmail.com>
2009-01-13 21:28:43 +0100

    First import of DiffEngine from: http://www.codeproject.com/KB/recipes/diffengine.aspx

---
+++ dotGit/Diff/Engine/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dotGit.Diff.Engine
+{
+  public enum DiffResultSpanStatus
+  {
+    NoChange,
+    Replace,
+    DeleteSource,
+    AddDestination
+  }
+
+  internal enum DiffStatus
+  {
+    Matched = 1,
+    NoMatch = -1,
+    Unknown = -2
+
+  }
+
+  public enum DiffEngineLevel
+  {
+    FastImperfect,
+    Medium,
+    SlowPerfect
+  }
+}
