5cd415bb8944dc0690419a6bd0211544b45e48c2
Mark Gerrits <m.gerrits@diract-it.nl>
2009-02-03 18:24:09 +0100

    Added a couple of Index handling tests, dictating the API

--- Test/IndexTests.cs
+++ Test/IndexTests.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 using dotGit;
 using dotGit.Index;
 using dotGit.Generic;
+using System.IO;
 
 namespace Test
 {
@@ -101,5 +102,51 @@ namespace Test
       IndexEntry entry = repo.Index.Entries[0];
       Assert.IsTrue(Sha.IsValidSha(entry.SHA), "IndexEntry.Sha should be a valid SHA-1");
     }
+
+    [Test]
+    [Category("Index Handling")]
+    public void StageFileShouldCreateNewIndexEntry()
+    {
+      string fileName = "TestFile-" + Guid.NewGuid().ToString();
+      string path = Path.Combine(Global.TestRepositoryPath, fileName);
+      File.WriteAllText(path, "THIS IS SOME TEXT");
+      
+      Sha newSha = repo.Stage(path);
+      IndexEntry entry = repo.Index[newSha];
+
+      Assert.IsNotNull(entry, "Newly added IndexEntry cannot be null");
+      Assert.IsTrue(repo.Index.StageIsNormal,"StageIsNormal is false, adding a new entry failed");
+      Assert.AreEqual(entry.Stage,IndexStage.Normal, "Newly added entry is not Stage==Normal");
+    }
+
+    [Test]
+    [Category("Index Handling")]
+    public void StageDirectoryShouldCreateNewIndexEntries()
+    {
+      string subPath = Path.Combine(Global.TestRepositoryPath,"NewSubDir");
+      int numberOfFiles = 10;
+
+      for (int idx = 0; idx < numberOfFiles; idx++)
+      {
+        string fileName = "TestFile-Dir-" + idx.ToString();
+        string path = Path.Combine(subPath, fileName);
+        File.WriteAllText(path, String.Format("THIS IS SOME TEXT, file {0}",idx));
+      }
+
+      List<Sha> newShas = repo.StageDirectory(subPath);
+
+      bool result = true;
+      foreach (Sha s in newShas)
+      {
+        if (!repo.Index.Contains(s) || repo.Index[s] == null)
+          result = false;
+      }
+
+      Assert.IsNotNull(newShas,"StageDirectory should return a List<Sha>");
+      Assert.AreEqual(numberOfFiles, newShas.Count, String.Format("StageDirectory result does not match number of files {0}",numberOfFiles));
+      Assert.IsTrue(repo.Index.StageIsNormal, "StageIsNormal is false, adding new entries failed");
+    }
+
+
   }
 }
