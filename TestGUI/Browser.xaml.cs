918ee237e94b6a7d895d33152d380a4bf291a266
henon <meinrad.recheis@gmail.com>
2009-06-12 00:23:12 +0200

    added blob content view to testgui. click on files in the tree to view their contents
    Signed-off-by: henon <meinrad.recheis@gmail.com>

--- TestGUI/Browser.xaml.cs
+++ TestGUI/Browser.xaml.cs
@@ -16,6 +16,7 @@ using dotGit.Objects;
 using System.Collections.ObjectModel;
 using Microsoft.Win32;
 using dotGit.Config;
+using dotGit.Refs;
 
 namespace TestGUI
 {
@@ -26,25 +27,55 @@ namespace TestGUI
         {
             InitializeComponent();
             m_commits.SelectionChanged += (o, args) => SelectCommit(m_commits.SelectedItem as Commit);
+            m_branches.SelectionChanged += (o, args) => SelectBranch(m_branches.SelectedItem as Branch);
+            m_tags.SelectionChanged += (o, args) => SelectTag(m_tags.SelectedItem as Tag);
+            m_tree.SelectedItemChanged += (o, args) => SelectObject(m_tree.SelectedValue as TreeNode);
             //m_config_tree.SelectedItemChanged += (o, args) => SelectConfiguration(m_config_tree.SelectedItem);
         }
+
+        private void  SelectObject(TreeNode node)
+        {
+            if (node.IsBlob)
+            {
+                var blob = node as Blob;
+                var text = Encoding.UTF8.GetString(blob.Content);
+                m_object.Document.Blocks.Clear();
+                var p = new Paragraph();
+                p.Inlines.Add(text);
+                m_object.Document.Blocks.Add(p);
+                m_object_title.Text = "Content of " + node.Path;
+            }
+            else
+            {
+                m_object.Document.Blocks.Clear();
+            }
+        }
+
 
         Repository m_repository;
 
 
         // load
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void OnLoadRepository(object sender, RoutedEventArgs e)
         {
             var url = m_url_textbox.Text;
             Repository repo = Repository.Open(url);
             m_repository = repo;
             m_branches.ItemsSource = repo.Branches;
             m_tags.ItemsSource = repo.Tags;
-            var list = repo.HEAD.Commit.Ancestors.ToList();
-            list.Insert(0, repo.HEAD.Commit);
-            m_commits.ItemsSource = list;
-            m_commits.SelectedIndex = 0;
+            DisplayCommit(repo.HEAD.Commit, "HEAD");
             ReloadConfiguration();
+        }
+
+        private void SelectBranch(Branch branch)
+        {
+            DisplayCommit(branch.Commit, "Branch "+branch.Name);
+        }
+
+        private void SelectTag(Tag tag)
+        {
+            if (tag.Object is Commit)
+                DisplayCommit(tag.Object as Commit, "Tag "+tag.Name);
         }
 
         private void OnSelectRepository(object sender, RoutedEventArgs e)
@@ -68,11 +99,23 @@ namespace TestGUI
         //    }
         //}
 
+        private void DisplayCommit(Commit commit, string info)
+        {
+            if (commit == null)
+                return;
+            var list = commit.Ancestors.ToList();
+            list.Insert(0, commit);
+            m_commits.ItemsSource = list;
+            m_commits.SelectedIndex = 0;
+            m_commit_title.Text = "Commit history for " + info;
+        }
+
         private void SelectCommit(Commit commit)
         {
             if (commit == null)
                 return;
             m_tree.ItemsSource = commit.Tree.Children;
+            m_tree_title.Text = "Repository tree of Commit " + commit.SHA;
             //(m_tree.ItemContainerGenerator.ContainerFromIndex(0) as TreeViewItem).IsExpanded = true;
         }
 
