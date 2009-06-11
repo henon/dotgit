12ffa0bb269adbbe24cbcf5230e7ae3cb26e77d6
henon <meinrad.recheis@gmail.com>
2009-06-11 13:52:10 +0200

    added a very simple config editor to the testgui
    Signed-off-by: henon <meinrad.recheis@gmail.com>

--- TestGUI/Browser.xaml.cs
+++ TestGUI/Browser.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Shapes;
 using dotGit;
 using dotGit.Objects;
 using System.Collections.ObjectModel;
+using Microsoft.Win32;
+using dotGit.Config;
 
 namespace TestGUI
 {
@@ -24,7 +26,47 @@ namespace TestGUI
         {
             InitializeComponent();
             m_commits.SelectionChanged += (o, args) => SelectCommit(m_commits.SelectedItem as Commit);
+            //m_config_tree.SelectedItemChanged += (o, args) => SelectConfiguration(m_config_tree.SelectedItem);
         }
+
+        Repository m_repository;
+
+
+        // load
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var url = m_url_textbox.Text;
+            Repository repo = Repository.Open(url);
+            m_repository = repo;
+            m_branches.ItemsSource = repo.Branches;
+            m_tags.ItemsSource = repo.Tags;
+            var list = repo.HEAD.Commit.Ancestors.ToList();
+            list.Insert(0, repo.HEAD.Commit);
+            m_commits.ItemsSource = list;
+            m_commits.SelectedIndex = 0;
+            ReloadConfiguration();
+        }
+
+        private void OnSelectRepository(object sender, RoutedEventArgs e)
+        {
+            var dlg = new System.Windows.Forms.FolderBrowserDialog();
+            //dlg.CheckPathExists = true;
+            if (dlg.ShowDialog() ==  System.Windows.Forms.DialogResult.OK)
+            {
+                m_url_textbox.Text = dlg.SelectedPath;
+            }
+        }
+
+        //private void SelectConfiguration(object obj)
+        //{
+        //    if (obj is Entry)
+        //    {
+        //        var entry = obj as dotGit.Config.Entry;
+        //        m_config_name.Content = entry.FullName;
+        //        if (entry.Value != null)
+        //            m_config_value.Text = entry.Value;
+        //    }
+        //}
 
         private void SelectCommit(Commit commit)
         {
@@ -34,18 +76,27 @@ namespace TestGUI
             //(m_tree.ItemContainerGenerator.ContainerFromIndex(0) as TreeViewItem).IsExpanded = true;
         }
 
-        // load
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void OnLoadConfiguration(object sender, RoutedEventArgs e)
         {
-            var url = m_url_textbox.Text;
-            Repository repo = Repository.Open(url);
-            m_branches.ItemsSource = repo.Branches;
-            m_tags.ItemsSource = repo.Tags;
-            var list = repo.HEAD.Commit.Ancestors.ToList();
-            list.Insert(0, repo.HEAD.Commit);
-            m_commits.ItemsSource = list;
-            m_commits.SelectedIndex = 0;
+            ReloadConfiguration();
         }
 
+        private void ReloadConfiguration()
+        {
+            m_repository.Config.Reload();
+            m_config_tree.ItemsSource = null;
+            m_config_tree.ItemsSource = m_repository.Config.Sections;
+        }
+
+        private void SaveConfiguration()
+        {
+            m_repository.Config.Save();
+            ReloadConfiguration();
+        }
+
+        private void OnSaveConfiguration(object sender, RoutedEventArgs e)
+        {
+            SaveConfiguration();
+        }
     }
 }
